88244036f2ebcf6eca4755865673352bea601f1c
AndreRongcales19 <302334829@student.rocmondriaan.nl>
2025-05-13 12:59:16 +0200

    Update config.php

--- ai-recipe-wrapper/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Configuratiebestand voor API-instellingen
-define('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', 'YOUR_API_KEY_HERE'); // In een latere les vervangen
-?>
